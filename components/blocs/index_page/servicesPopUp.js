d6dcfbb81116f0193c95cdc9fd26781a3b5e252e
fredowagon <fedde.leg@gmail.com>
2021-10-12 19:15:12 +0200

    mise à jour bloc service

--- components/blocs/index_page/servicesPopUp.js
+++ components/blocs/index_page/servicesPopUp.js
@@ -7,19 +7,23 @@ import maintenance from '../../../public/illu_maint.png'
 import refercenement from '../../../public/illu_refer.png'
 import testIllu from "../../../public/test_illu_app.png"
 import testcommerce from "../../../public/test_ecommerce.png"
+import test_illu_web from "../../../public/test_illu_web.png"
 import testwebsite from "../../../public/test_website.png"
+import test_icon_app from "../../../public/test_icon_app.svg"
+import test_icon_shop from "../../../public/test_icon_shop.svg"
+import test_icon_site from "../../../public/test_icon_site.svg"
+import wepapp_illustration from "../../../public/webapp_illustration.png"
+import test_illu from '../../../public/landing_illu.svg'
 import shop from "../../../public/testshop.svg"
 import page from '../../../public/testpage.svg'
 import app from '../../../public/testapp.svg'
 import commerce from '../../../public/illu_commerce.png'
 import Image from "next/image";
+
 import Link from "next/link";
 import {useState} from "react";
 
 export default function ServicesPopUp() {
-
-
-
 
     return (
         <div className={style.services_popup__container}>
@@ -31,13 +35,20 @@ export default function ServicesPopUp() {
                 <FadeTranslateTopWhenVisible tresholdOption={0} animation="slow">
                 <div className={style.services_popup__content}>
                     <Link href="/services/sitesweb">
-                    <div className={style.services_popup__item} >
-                        <div className={style.background_animation}></div>
-                        <div className={style.popup_images}>
-                            <Image src={page}/>
+                    <div className={`${style.popup_item__green} ${style.services_popup__item}`} >
+                        <div className={style.services_popup__image}>
+                            <Image src={test_icon_site}/>
+
                         </div>
-                        <h3>Création web</h3>
-                        <p>Un bon site Web est bien plus qu'une carte de visite numérique.</p>
+
+                        <div className={style.services_popup__title}>
+
+
+                            <h3>Création web
+                                <span className={ `${style.popup_underline__green} ${style.popup_underline}`}/></h3>
+
+                        </div>
+                        <p>Un bon site Web est bien plus qu'une carte de visite numérique. <br/> Site internet, vitrine.</p>
                         <div className={`${style.popup_more_info} `}>
                             <span></span>
                             <p>Voir plus</p>
@@ -48,13 +59,18 @@ export default function ServicesPopUp() {
 
 
                     <Link href="/services/applicationsweb">
-                    <div className={style.services_popup__item}>
-                        <div className={style.background_animation}></div>
-                        <div className={style.popup_images}>
-                            <Image src={app}/>
+                    <div className={`${style.popup_item__blue} ${style.services_popup__item}`}>
+                        <div className={style.services_popup__image}>
+                            <Image src={test_icon_app}/>
+
                         </div>
-                        <h3>Applications Web</h3>
-                        <p>Créez de meilleures affaires avec une application Web conviviale.</p>
+                        <div className={style.services_popup__title}>
+
+                            <h3>Applications Web
+                                <span className={`${style.popup_underline__blue} ${style.popup_underline}`}/></h3>
+                        </div>
+
+                        <p>Créez de meilleures affaires avec une application Web conviviale. <br/>Application sur mesure, PWA</p>
                         <div className={`${style.popup_more_info} `}>
                             <span></span>
                             <p>Voir plus</p>
@@ -65,18 +81,26 @@ export default function ServicesPopUp() {
 
                     <Link href="/services/ecommerce">
 
-                        <div className={style.services_popup__item}>
-                            <div className={style.background_animation}></div>
-                            <div className={style.popup_images}>
-                                <Image src={shop}/>
+                        <div className={`${style.popup_item__yellow} ${style.services_popup__item}`}>
+                            <div className={style.services_popup__image}>
+                                <Image src={test_icon_shop}/>
+
                             </div>
-                            <h3>Site E-Commerce</h3>
+                            <div className={style.services_popup__title}>
+
+                                <h3>Ecommerce
+                                    <span className={`${style.popup_underline__yellow} ${style.popup_underline}`}/></h3>
+
+                            </div>
+
+
                             <p>Révolutionnez votre chiffre d'affaires avec une solution e-commerce moderne.</p>
                             <div className={`${style.popup_more_info} `}>
                                 <span></span>
                                 <p>Voir plus</p>
 
                             </div>
+
                         </div>
                     </Link>
 
