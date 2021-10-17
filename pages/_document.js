d1f1ec5e47a237fb397082ff23745e27ad3f05b7
fredowagon <fedde.leg@gmail.com>
2021-10-17 10:38:30 +0200

    ajout meta viewport

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
         return (
             <Html lang="fr">
                 <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"/>
                     {/*google font*/}
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
