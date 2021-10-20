ae880b00299f7067a3db2fe28427081b3c082ad6
fredowagon <fedde.leg@gmail.com>
2021-10-20 13:20:31 +0200

    test encore plugin sitemap netlify

--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.pokouweb.fr/',
-    generateRobotsTxt: true, // (optional)
-    exclude: ['/api/*'],
-    // ...other options
-}
