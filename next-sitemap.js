e559b91bf56b2cea35026c1f80f52aac4bbdff14
fredowagon <fedde.leg@gmail.com>
2021-10-20 12:07:37 +0200

    test de next sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.pokouweb.fr/',
+    generateRobotsTxt: true, // (optional)
+    exclude: ['/api/*'],
+    // ...other options
+}
