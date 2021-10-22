6b5d03efd38fae2155f44d3724863601f24a31d6
fredowagon <fedde.leg@gmail.com>
2021-10-22 14:10:51 +0200

    tessss

--- netlify/functions/netlifyContact.js
+++ netlify/functions/netlifyContact.js
@@ -5,6 +5,6 @@ exports.handler = function (event, context) {
 
  return {
      statusCode: 200,
-     body: `${event.body ? JSON.stringify(event.body) : ""}`
+     body: `${event ? JSON.stringify(event) : ""}`
  }
 }
