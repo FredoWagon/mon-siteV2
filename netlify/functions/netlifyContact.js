5e826b455f0a390e3530b072d0a6ce241be38c90
fredowagon <fedde.leg@gmail.com>
2021-10-22 14:17:37 +0200

    test putain

--- netlify/functions/netlifyContact.js
+++ netlify/functions/netlifyContact.js
@@ -1,12 +1,7 @@
 
-
-exports.handler = function (event, context) {
-
-
-
-
- return {
-     statusCode: 200,
-     body: "salut"
- }
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
 }
