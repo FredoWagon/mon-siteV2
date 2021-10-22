d80101a1b19965ae198c0691fa758ed4e83e9ba6
fredowagon <fedde.leg@gmail.com>
2021-10-22 15:08:03 +0200

    test postman

--- netlify/functions/netlifyContact.js
+++ netlify/functions/netlifyContact.js
@@ -1,9 +1,9 @@
 
 
 exports.handler = async function(event, context) {
-    const body = JSON.parse(event.body)
+    const eventBody = JSON.parse(event.body)
     return {
         statusCode: 200,
-        body: JSON.stringify(body)
+        body: JSON.stringify(eventBody)
     };
 }
