015f4276d12c5d9bb9f3672867b8c38bc0c37869
fredowagon <fedde.leg@gmail.com>
2021-10-22 15:25:51 +0200

    lllllll

--- netlify/functions/netlifyContact.js
+++ netlify/functions/netlifyContact.js
@@ -1,10 +1,20 @@
 
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event, context, callback) {
     const eventBody = JSON.parse(event.body)
 
-    return {
-        statusCode: 200,
-        body: eventBody.body
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Max-Age': 2592000,
+        'Access-Control-Allow-Credentials': true,
     };
+
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(eventBody),
+        headers,
+    });
 }
