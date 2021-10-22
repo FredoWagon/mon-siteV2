f980536cb35f604f737341c527880be79273d1bb
fredowagon <fedde.leg@gmail.com>
2021-10-22 15:55:02 +0200

    ola ?

--- netlify/functions/netlifyContact.js
+++ netlify/functions/netlifyContact.js
@@ -1,7 +1,39 @@
+const nodemailer = require('nodemailer');
+
+const feddeTransporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.PRIVATE_MAIL,
+        pass: process.env.PRIVATE_MAIL_PASSWORD,
+    },
+    secure: true
+});
+
 
 
 exports.handler = async function(event, context, callback) {
-    const eventBody = JSON.parse(event.body)
+    const reqbody = JSON.parse(event.body)
+
+
+    const PokouMailData = {
+        from: `${reqbody.name} Via Poukou Web <${process.env.POKOU_MAIL}>`,
+        to: process.env.POKOU_MAIL,
+        replyTo: `${reqbody.name} <${reqbody.email}>`,
+        subject: `Nouveau message !`,
+        text: reqbody.message + " | Sent from: " + reqbody.name + reqbody.email + reqbody.phone,
+        html: `<div>${reqbody.message}</div> <br/>
+<br/>
+<span>${reqbody.name}</span>
+<br/>
+<br/>
+<span>_____</span><br/>
+<span>Email: <span>${reqbody.email}</span></span> <br/>
+<span>Téléphone: <span>${reqbody.phone ? reqbody.phone : "..."}</span></span>`
+    }
+
+   const email = await feddeTransporter.sendMail(PokouMailData)
+
 
     const headers = {
         'Access-Control-Allow-Origin': '*',
@@ -14,7 +46,7 @@ exports.handler = async function(event, context, callback) {
 
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(eventBody),
+        body: email.messageId,
         headers,
     });
 }
