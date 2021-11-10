f2952251bad63c42584544f7394f9e1b3642b8eb
fredowagon <fedde.leg@gmail.com>
2021-11-10 14:42:02 +0100

    fix css

--- pages/404.js
+++ pages/404.js
@@ -10,12 +10,11 @@ export default function Custom404() {
     const oupsText = useRef(null);
 
     useEffect(() => {
+        imageAnimation.current.classList.remove(`${style.image_animation}`)
         const currentPage = window.location.pathname.replace("/", "")
-
         setPageName(currentPage)
         console.log(currentPage)
         animation();
-
     })
 
     const animation = () => {
